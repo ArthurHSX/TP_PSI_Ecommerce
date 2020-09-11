7b4466e20e7d8700a8846f1f3d8c5938b8a23b59
Arthur Santos <arthurwesley7@gmail.com>
2020-09-10 23:23:44 -0300

    Comecei a criar a base do sistema, adicionei duas classes Usuario e Anuncio na pasta Models.

---
+++ Ecommerce/Ecommerce/Models/Anuncio.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Models
+{
+    public class Anuncio
+    {
+        #region Propriedades
+
+        public int ID { get; set; }
+        public string TituloAnuncio { get; set; }
+        public string Descricao { get; set; }
+        public double Valor { get; set; }
+        // IMPLEMENTAR AS CLASSES BASE
+        //public int IDUsuario { get; set; }
+        //public int IDCategoria { get; set; }
+        //public int IDNota { get; set; }
+        //public int IDEndereco { get; set; }
+
+        #endregion
+
+        #region Métodos
+
+        public void ManterAnuncio()
+        {
+            throw new Exception("Implemmentar método Salvar, Excluir");
+        }
+
+        public void BuscarFotos(int idAnuncio)
+        {
+            throw new Exception("Implemmentar método de busca");
+        }
+
+        #endregion
+    }
+}
