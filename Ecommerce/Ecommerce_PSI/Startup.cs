2f159c7662905f71c9248c75581a9b9c472b6c1d
Gustavo Caldeira <gusteitiom@hotmail.com>
2020-09-24 20:52:59 -0300

    Commit inicial com tela de login

--- Ecommerce/Ecommerce_PSI/Startup.cs
+++ Ecommerce/Ecommerce_PSI/Startup.cs
@@ -15,6 +15,7 @@ namespace Ecommerce_PSI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,7 +28,14 @@ namespace Ecommerce_PSI
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                app.UseStaticFiles();
+                app.UseMvc(routes =>
+                {
+                    routes.MapRoute(
+                        name: "Login", 
+                        template: "{controller=Home}/{action=Login}/{id?}"
+                    );
+                });
             });
         }
     }
--- Ecommerce/Ecommerce_PSI/obj/Debug/netcoreapp2.1/Ecommerce_PSI.RazorTargetAssemblyInfo.cs
+++ Ecommerce/Ecommerce_PSI/obj/Debug/netcoreapp2.1/Ecommerce_PSI.RazorTargetAssemblyInfo.cs
@@ -11,12 +11,14 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ProvideApplicationPartFactoryAttribute("Microsoft.AspNetCore.Mvc.ApplicationParts.CompiledRazorAssemblyApplicationPartFac" +
+    "tory, Microsoft.AspNetCore.Mvc.Razor")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce_PSI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyProductAttribute("Ecommerce_PSI")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Ecommerce_PSI")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce_PSI")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce_PSI.Views")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
