b484440740a4349f8719ee569c0c00ecb8c7ea5d
Arthur Santos <arthurwesley7@gmail.com>
2020-10-03 10:50:02 -0300

    Revert "Att"
    This reverts commit cfb8673d326e513b89dde71f8106edce89789903.

--- Ecommerce/Ecommerce_PSI/Context/EcommerceContext.cs
+++ Ecommerce/Ecommerce_PSI/Context/EcommerceContext.cs
@@ -9,22 +9,11 @@ namespace Ecommerce.Context
         public EcommerceContext() : base()      
         {}
 
-        public EcommerceContext(DbContextOptions<EcommerceContext> opcoes) : base(opcoes) { }
-
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-        }
+        public DbSet<Usuario> Usuario { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=Ecommerce_PSI;Trusted_Connection=True;");
         }
-
-        #region [ Tables ]
-
-        public DbSet<Usuario> Usuario { get; set; }
-
-        #endregion
     }
 }
